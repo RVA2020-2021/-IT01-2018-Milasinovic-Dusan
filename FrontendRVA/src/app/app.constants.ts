603e8289093d34a827813228c4b766120d0a409e
dusanmila <65555511+dusanmila@users.noreply.github.com>
2021-08-13 18:12:52 +0200

    frontend commit

---
+++ FrontendRVA/src/app/app.constants.ts
@@ -0,0 +1,13 @@
+/*
+export    const SMER_URL = 'https://rva-backendit142018.herokuapp.com/smer';
+export const PROJEKAT_URL = 'https://rva-backendit142018.herokuapp.com/projekat';
+export const GRUPA_URL = 'https://rva-backendit142018.herokuapp.com/grupa';
+export const STUDENT_URL = 'https://rva-backendit142018.herokuapp.com/student';
+export const STUDENTI_GRUPE = 'https://rva-backendit142018.herokuapp.com/studentiGrupa';
+*/
+export const SMER_URL = 'http://localhost:8083/smer';
+export const PROJEKAT_URL = 'http://localhost:8083/projekat';
+export const GRUPA_URL = 'http://localhost:8083/grupa';
+export const STUDENT_URL = 'http://localhost:8083/student';
+
+
